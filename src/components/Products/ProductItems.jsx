404a4a982b90fb7d119da734c769a2f03db1278b
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-08-06 17:59:45 +0530

    products showed

--- src/components/Products/ProductItems.jsx
+++ src/components/Products/ProductItems.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './productItems.css'
 function ProductItems({product}) {
+    const truncateText = (text, maxLength) => {
+        if (text.length > maxLength) {
+          return text.substring(0, maxLength) + '...';
+        }
+        return text;
+      };
+    
   return (
     <div className='product-item'>
         <img className='product-image' src={product.image} width='100%' height='80%'/>
-        <span className='product-title'>{product.title}</span>
-        <span className='product-description'>{product.description}</span>
+        <span className='product-title'>{truncateText(product.title, 20)}</span> <br />
+        <span className='product-description'>{truncateText(product.description, 35)}</span>
     </div>
   )
 }
